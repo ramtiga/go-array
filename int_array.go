b0f69c77fd71be8ee623e8e12c1034dea274f530
ramtiga <dhanegm731@gmail.com>
2014-06-22 01:47:52 +0900

    gofmt

--- int_array.go
+++ int_array.go
@@ -1,172 +1,169 @@
 package array
 
 import (
-    "fmt"
-    "sort"
-    "strings"
+	"fmt"
+	"sort"
+	"strings"
 )
 
 type IntArray []int
 
 func (p IntArray) Len() int {
-    return len(p)
+	return len(p)
 }
 
 func (p IntArray) Less(i, j int) bool {
-    return p[i] < p[j]
+	return p[i] < p[j]
 }
 
 func (p IntArray) Swap(i, j int) {
-    p[i], p[j] = p[j], p[i]
+	p[i], p[j] = p[j], p[i]
 }
-
-
 
 // length of the array
 func (p *IntArray) Length() int {
-    return len(*p)
+	return len(*p)
 }
 
 // length of the array
 func (p *IntArray) Size() int {
-    return len(*p)
+	return len(*p)
 }
 
 // add to the existing array
 func (p *IntArray) Push(value ...int) {
-    tmp := *p
-    for _, v := range value {
-        tmp = append(tmp, v)
-    }
-    *p = tmp
+	tmp := *p
+	for _, v := range value {
+		tmp = append(tmp, v)
+	}
+	*p = tmp
 }
 
 // remove last value of the array
 func (p *IntArray) Pop() int {
-    tmp := *p
-    last := tmp[len(tmp)-1:len(tmp)]
-    tmp = tmp[0:len(tmp)-1]
+	tmp := *p
+	last := tmp[len(tmp)-1 : len(tmp)]
+	tmp = tmp[0 : len(tmp)-1]
 
-    *p = tmp
-    return last[0]
+	*p = tmp
+	return last[0]
 }
 
 // get first value of the array
 func (p IntArray) First() int {
-    return p[0]
+	return p[0]
 }
 
 // get last value of the array
 func (p IntArray) Last() int {
-    last := p[len(p)-1:len(p)]
-    return last[0]
+	last := p[len(p)-1 : len(p)]
+	return last[0]
 }
 
 // remove first value of the array
 func (p *IntArray) Shift() {
-    tmp := *p
-    if len(tmp) < 1 {
-        return
-    }
-    *p = tmp[1:len(tmp)]
+	tmp := *p
+	if len(tmp) < 1 {
+		return
+	}
+	*p = tmp[1:len(tmp)]
 }
 
 // add value the first index to the existing array
 func (p *IntArray) Unshift(arry IntArray) {
-    tmp := *p
-    arry.Concat(tmp)
-    *p = arry
+	tmp := *p
+	arry.Concat(tmp)
+	*p = arry
 }
 
 // add new array to the existing array
 func (p *IntArray) Concat(arry IntArray) {
-    tmp := *p
-    tmp = append(tmp, arry...)
-    *p = tmp
+	tmp := *p
+	tmp = append(tmp, arry...)
+	*p = tmp
 }
 
 // initialize of the array
 func (p *IntArray) Clear() {
-    var tmp IntArray
-    *p = tmp
+	var tmp IntArray
+	*p = tmp
 }
 
 // remove del_target from array
 func (p *IntArray) Delete(del_value int) {
-    tmp := *p
-    var new_array IntArray
-    for i:=0; i < len(tmp); i++ {
-        if tmp[i] != del_value {
-            new_array = append(new_array, tmp[i])
-        }
-    }
-    *p = new_array
+	tmp := *p
+	var new_array IntArray
+	for i := 0; i < len(tmp); i++ {
+		if tmp[i] != del_value {
+			new_array = append(new_array, tmp[i])
+		}
+	}
+	*p = new_array
 }
 
 // remove del_index of index from array
 func (p *IntArray) Delete_at(del_index int) {
-    tmp := *p
-    var new_array IntArray
-    for i:=0; i < len(tmp); i++ {
-        if i != del_index {
-            new_array = append(new_array, tmp[i])
-        }
-    }
-    *p = new_array
+	tmp := *p
+	var new_array IntArray
+	for i := 0; i < len(tmp); i++ {
+		if i != del_index {
+			new_array = append(new_array, tmp[i])
+		}
+	}
+	*p = new_array
 }
 
 // get index of value from array
 func (p IntArray) Index(n int) int {
-    for i, v := range p {
-        if v == n {
-            return i
-        }
-    }
-    return -1
+	for i, v := range p {
+		if v == n {
+			return i
+		}
+	}
+	return -1
 }
 
 // get a new reverse array
 func (p *IntArray) Reverse() {
-    tmp := *p
-    var new_array IntArray
-    length := len(tmp)
+	tmp := *p
+	var new_array IntArray
+	length := len(tmp)
 
-    for i:=length-1; i >= 0; i-- {
-        new_array = append(new_array, tmp[i])
-    }
-    *p = new_array
+	for i := length - 1; i >= 0; i-- {
+		new_array = append(new_array, tmp[i])
+	}
+	*p = new_array
 }
 
-// get a new string from array 
+// get a new string from array
 func (p *IntArray) Join(sep string) string {
-    tmp := *p
-    var str_ary []string
+	tmp := *p
+	var str_ary []string
 
-    for _, v:= range tmp {
-        str_ary = append(str_ary, fmt.Sprint(v))
-    }
-    str := strings.Join(str_ary, sep)
-    return str
+	for _, v := range tmp {
+		str_ary = append(str_ary, fmt.Sprint(v))
+	}
+	str := strings.Join(str_ary, sep)
+	return str
 }
 
 // get a new unique array from array
 func (p *IntArray) Uniq() IntArray {
-    tmp := *p
-    var new_array IntArray
+	tmp := *p
+	var new_array IntArray
 
-    for _, v := range tmp {
-        if new_array.Index(v) == -1 {
-            new_array = append(new_array, v)
-        }
-    }
-    return new_array
+	for _, v := range tmp {
+		if new_array.Index(v) == -1 {
+			new_array = append(new_array, v)
+		}
+	}
+	return new_array
 }
 
 // get a new sort array from array
-func (p *IntArray) Sort() { 
-    tmp := *p
-    // var tmp2 IntArray
-    sort.Sort(tmp)
-    *p = tmp
+func (p *IntArray) Sort() {
+	tmp := *p
+	// var tmp2 IntArray
+	sort.Sort(tmp)
+	*p = tmp
 }
-
--- int_array_test.go
+++ int_array_test.go
@@ -1,196 +1,193 @@
 package array
 
 import (
-    "testing"
+	"testing"
 )
 
 func TestLength(t *testing.T) {
-    var d IntArray = IntArray{5, 3, 1, 2, 6}    
-    result := d.Length()
-    if result != 5 {
-        t.Errorf("result=%v", result)
-    }
+	var d IntArray = IntArray{5, 3, 1, 2, 6}
+	result := d.Length()
+	if result != 5 {
+		t.Errorf("result=%v", result)
+	}
 }
 
 func TestSize(t *testing.T) {
-    var d IntArray = IntArray{5, 3, 1, 2, 6}    
-    result := d.Size()
-    if result != 5 {
-        t.Errorf("result=%v", result)
-    }
+	var d IntArray = IntArray{5, 3, 1, 2, 6}
+	result := d.Size()
+	if result != 5 {
+		t.Errorf("result=%v", result)
+	}
 }
 
 func TestPush(t *testing.T) {
-    var d IntArray = IntArray{5, 3, 1, 2, 6}    
-    chk := IntArray{5, 3, 1, 2, 6, 7}
-    d.Push(7)
-    
-    for i, _ := range d {
-       if d[i] != chk[i] {
-          t.Errorf("result=%v, index=%v", d, i)
-       } 
-    }
+	var d IntArray = IntArray{5, 3, 1, 2, 6}
+	chk := IntArray{5, 3, 1, 2, 6, 7}
+	d.Push(7)
+
+	for i, _ := range d {
+		if d[i] != chk[i] {
+			t.Errorf("result=%v, index=%v", d, i)
+		}
+	}
 }
 
 func TestFirst(t *testing.T) {
-    var d IntArray = IntArray{5, 3, 1, 2, 6}    
-    resutl := d.First()
-    
-    if resutl != 5 {
-        t.Errorf("result=%v", d)
-    } 
+	var d IntArray = IntArray{5, 3, 1, 2, 6}
+	resutl := d.First()
+
+	if resutl != 5 {
+		t.Errorf("result=%v", d)
+	}
 }
 
 func TestLast(t *testing.T) {
-    var d IntArray = IntArray{5, 3, 1, 2, 6}    
-    resutl := d.Last()
-    
-    if resutl != 6 {
-        t.Errorf("result=%v", d)
-    } 
+	var d IntArray = IntArray{5, 3, 1, 2, 6}
+	resutl := d.Last()
+
+	if resutl != 6 {
+		t.Errorf("result=%v", d)
+	}
 }
 
 func TestShift(t *testing.T) {
-    var d IntArray = IntArray{5, 3, 1, 2, 6}    
-    chk := IntArray{3, 1, 2, 6}    
-    d.Shift()
-    
-    for i, _ := range d {
-       if d[i] != chk[i] {
-          t.Errorf("result=%v, index=%v", d, i)
-       } 
-    }
+	var d IntArray = IntArray{5, 3, 1, 2, 6}
+	chk := IntArray{3, 1, 2, 6}
+	d.Shift()
+
+	for i, _ := range d {
+		if d[i] != chk[i] {
+			t.Errorf("result=%v, index=%v", d, i)
+		}
+	}
 }
 
 func TestUnshift(t *testing.T) {
-    var d IntArray = IntArray{5, 3, 1, 2, 6}    
-    chk := IntArray{10, 11, 5, 3, 1, 2, 6}    
+	var d IntArray = IntArray{5, 3, 1, 2, 6}
+	chk := IntArray{10, 11, 5, 3, 1, 2, 6}
 
-    add := IntArray{10, 11}
-    d.Unshift(add)
-    
-    for i, _ := range d {
-       if d[i] != chk[i] {
-          t.Errorf("result=%v, index=%v", d, i)
-       } 
-    }
+	add := IntArray{10, 11}
+	d.Unshift(add)
+
+	for i, _ := range d {
+		if d[i] != chk[i] {
+			t.Errorf("result=%v, index=%v", d, i)
+		}
+	}
 }
 
 func TestConcat(t *testing.T) {
-    var d IntArray = IntArray{5, 3, 1, 2, 6}    
-    chk := IntArray{5, 3, 1, 2, 6, 10, 11}    
+	var d IntArray = IntArray{5, 3, 1, 2, 6}
+	chk := IntArray{5, 3, 1, 2, 6, 10, 11}
 
-    add := IntArray{10, 11}
-    d.Concat(add)
-    
-    for i, _ := range d {
-       if d[i] != chk[i] {
-          t.Errorf("result=%v, index=%v", d, i)
-       } 
-    }
+	add := IntArray{10, 11}
+	d.Concat(add)
+
+	for i, _ := range d {
+		if d[i] != chk[i] {
+			t.Errorf("result=%v, index=%v", d, i)
+		}
+	}
 }
 
 func TestClear(t *testing.T) {
-    var d IntArray = IntArray{5, 3, 1, 2, 6}    
-    d.Clear()
-    if d.Length() != 0 {
-        t.Errorf("result=%v", d)
-    } 
+	var d IntArray = IntArray{5, 3, 1, 2, 6}
+	d.Clear()
+	if d.Length() != 0 {
+		t.Errorf("result=%v", d)
+	}
 }
 
 func TestDelete(t *testing.T) {
-    var d IntArray = IntArray{5, 3, 1, 2, 6, 3}    
-    chk := IntArray{5, 1, 2, 6}
+	var d IntArray = IntArray{5, 3, 1, 2, 6, 3}
+	chk := IntArray{5, 1, 2, 6}
 
-    d.Delete(3)
-    
-    for i, _ := range d {
-       if d[i] != chk[i] {
-          t.Errorf("result=%v, index=%v", d, i)
-       } 
-    }
+	d.Delete(3)
+
+	for i, _ := range d {
+		if d[i] != chk[i] {
+			t.Errorf("result=%v, index=%v", d, i)
+		}
+	}
 }
 
 func TestDelete_at(t *testing.T) {
-    var d IntArray = IntArray{5, 3, 1, 2, 6}    
-    chk := IntArray{5, 1, 2, 6}
+	var d IntArray = IntArray{5, 3, 1, 2, 6}
+	chk := IntArray{5, 1, 2, 6}
 
-    d.Delete_at(1)
-    
-    for i, _ := range d {
-       if d[i] != chk[i] {
-          t.Errorf("result=%v, index=%v", d, i)
-       } 
-    }
+	d.Delete_at(1)
+
+	for i, _ := range d {
+		if d[i] != chk[i] {
+			t.Errorf("result=%v, index=%v", d, i)
+		}
+	}
 }
 
 func TestIndex(t *testing.T) {
-    var d IntArray = IntArray{5, 3, 1, 2, 6}    
+	var d IntArray = IntArray{5, 3, 1, 2, 6}
 
-    result := d.Index(1)
-    
-    if result == 3 {
-        t.Errorf("result=%v", result)
-    } 
-    result = d.Index(7)
-    
-    if result != -1 {
-        t.Errorf("result=%v", result)
-    } 
+	result := d.Index(1)
+
+	if result == 3 {
+		t.Errorf("result=%v", result)
+	}
+	result = d.Index(7)
+
+	if result != -1 {
+		t.Errorf("result=%v", result)
+	}
 }
 
 func TestReverse(t *testing.T) {
-    var d IntArray = IntArray{5, 3, 1, 2, 6}    
-    chk := IntArray{6, 2, 1, 3, 5}
+	var d IntArray = IntArray{5, 3, 1, 2, 6}
+	chk := IntArray{6, 2, 1, 3, 5}
 
-    d.Reverse()
-    
-    for i, _ := range d {
-       if d[i] != chk[i] {
-          t.Errorf("result=%v, index=%v", d, i)
-       } 
-    }
+	d.Reverse()
+
+	for i, _ := range d {
+		if d[i] != chk[i] {
+			t.Errorf("result=%v, index=%v", d, i)
+		}
+	}
 }
 
 func TestJoin(t *testing.T) {
-    var d IntArray = IntArray{5, 3, 1, 2, 6}    
-    chk := "5-3-1-2-6"
+	var d IntArray = IntArray{5, 3, 1, 2, 6}
+	chk := "5-3-1-2-6"
 
-    result := d.Join("-")
-    
-    if result != chk {
-        t.Errorf("result=%v", result)
-    } 
+	result := d.Join("-")
+
+	if result != chk {
+		t.Errorf("result=%v", result)
+	}
 }
 
 func TestUniq(t *testing.T) {
-    var d IntArray = IntArray{5, 3, 1, 3, 2, 1}
-    chk := IntArray{5, 3, 1, 2}
+	var d IntArray = IntArray{5, 3, 1, 3, 2, 1}
+	chk := IntArray{5, 3, 1, 2}
 
-    result := d.Uniq()
-    
-    if result.Length() != 4 {
-        t.Errorf("result.Length=%v", result.Length())
-    }
-    for i, _ := range result {
-       if result[i] != chk[i] {
-          t.Errorf("result=%v, index=%v", result, i)
-       } 
-    }
+	result := d.Uniq()
+
+	if result.Length() != 4 {
+		t.Errorf("result.Length=%v", result.Length())
+	}
+	for i, _ := range result {
+		if result[i] != chk[i] {
+			t.Errorf("result=%v, index=%v", result, i)
+		}
+	}
 }
 
 func TestSort(t *testing.T) {
-    var d IntArray = IntArray{5, 3, 1, 2, 6}    
-    chk := IntArray{1, 2, 3, 5, 6}
+	var d IntArray = IntArray{5, 3, 1, 2, 6}
+	chk := IntArray{1, 2, 3, 5, 6}
 
-    d.Sort()
-    
-    for i, _ := range d {
-       if d[i] != chk[i] {
-          t.Errorf("result=%v, index=%v", d, i)
-       } 
-    }
+	d.Sort()
+
+	for i, _ := range d {
+		if d[i] != chk[i] {
+			t.Errorf("result=%v, index=%v", d, i)
+		}
+	}
 }
-
-
-
